f2851509c575a8ded7903086bdb8201cc31a703f
DA\nwuser <goroumakisimamu810@gmail.com>
2017-11-10 22:35:51 +0900

    字幕を追加した

--- Kurisumasu/Kinect_Project/Assets/Scripts/Satou/Balloon.cs
+++ Kurisumasu/Kinect_Project/Assets/Scripts/Satou/Balloon.cs
@@ -1,32 +1,36 @@
 ﻿using System.Collections;
-
 using System.Collections.Generic;
-
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
 public class Balloon : MonoBehaviour
 {
+    //各セリフを入れる
+    public GameObject[] objBalloon;
+    /*
+      0 : 開幕セリフ01
+      1 : 開幕セリフ02
+      2 : 開幕セリフ03
+      3 : ツリーを出せるようになる時のセリフ
+      4 : ツリー出現時のセリフ
+      5 : 雪だるまを出せるようになる時のセリフ
+      6 : 雪だるま出現時のセリフ
+      7 : プレゼントを出せるようになる時のセリフ
+      8 : プレゼント出現時のセリフ
+      9 : ケーキを出せるようになる時のセリフ
+      10 : ケーキ出現時のセリフ
+      11 : 炎を出せるようになる時のセリフ
+      12 : ろうそくに着火時のセリフ
+      13 : ゲーム終了時のセリフ
+    */
 
-    public GameObject start_balloon;
-    public GameObject party_balloon;
-    public GameObject candle_balloon;
-    public GameObject pumpkin_balloon;
-    public GameObject candy_balloon;
-
-    public float b_time = 5;         //フキダシが消えるまでの時間
-    public float c_time = 5;         //フキダシが消えるまでの時間
-    public float d_time = 5;         //フキダシが消えるまでの時間
-    public float e_time = 5;         //フキダシが消えるまでの時間
-    public float f_time = 5;         //フキダシが消えるまでの時間
-
-    public bool s_balloon = false;   //最初のフキダシ
-
-    public bool pa_balloon = false;  //パーティ準備のフキダシ
-    public bool cd_balloon = false;  //キャンドルののフキダシ
-    public bool pu_balloon = false;  //カボチャのフキダシ
-    public bool cn_balloon = false;  //飴のフキダシ
+    //各セリフのアクティブを切り替えるトリガー
+    public int[] trgBalloon = new int[13];
+    /*
+      0 = false
+      1 = true    
+    */
 
     //時間経過
     public float gameTime = 0;
@@ -40,105 +44,6 @@ public class Balloon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (FindObjectOfType<BodySourceView>().startFlg == true)
-        {
-            gameTime += Time.deltaTime;
-
-            //パーティーの吹き出しが出るまでの時間
-            if (gameTime >= 0 && s_balloon == true)
-            {
-                pa_balloon = true;
-                s_balloon = false;
-            }
-
-            if (s_balloon == false)
-            {
-                if (b_time == 5)
-                {
-                    start_balloon.SetActive(true);
-                }
-
-                b_time -= Time.deltaTime;
-
-                if (b_time <= 0)
-                {
-                    s_balloon = true;
-                    b_time = 6;     //フキダシが消えるまでの時間のリセット
-                }
-            }
-            if (b_time == 6)
-            {
-                start_balloon.SetActive(false);   //フキダシの削除
-            }
-
-            if (pa_balloon == true)
-            {
-                if (c_time == 5)
-                {
-                    party_balloon.SetActive(true);
-                }
-
-                c_time -= Time.deltaTime;
-
-                if (c_time <= 0)
-                {
-                    party_balloon.SetActive(false);
-                    pa_balloon = false;
-                    cd_balloon = true;
-                    c_time = 6;     //フキダシが消えるまでの時間のリセット
-                }
-            }
-
-            if (cd_balloon == true)
-            {
-                if (d_time == 5)
-                {
-                    candle_balloon.SetActive(true);
-                }
-
-                d_time -= Time.deltaTime;
-
-                if (d_time <= 4)
-                {
-                    cd_balloon = false;
-                    d_time = 6;     //フキダシが消えるまでの時間のリセット
-                                    //candle_balloon.SetActive(false);
-                }
-            }
-
-            if (pu_balloon == true)
-            {
-                if (e_time == 5)
-                {
-                    pumpkin_balloon.SetActive(true);
-                }
-                e_time -= Time.deltaTime;
-                if (e_time <= 4)
-                {
-                    //ここの文を「両方のロウソクに火が付いたとき」に変える、文の中の内容はそのままでよい
-                    pu_balloon = false;
-                    e_time = 6;     //フキダシが消えるまでの時間のリセット
-                                    //pumpkin_balloon.SetActive(false);
-                }
-            }
-
-            if (cn_balloon == true)
-            {
-                if (f_time == 5)
-                {
-                    candy_balloon.SetActive(true);
-                }
-                f_time -= Time.deltaTime;
-                if (f_time <= 4)
-                {
-
-                    //ここの文を「飴を降らせたとき」に変える、文の中の内容はそのままでよい、飴が画面いっぱいになる時間を待つなら時間待つ処理を加える
-                    cn_balloon = false;
-                    f_time = 6;     //フキダシが消えるまでの時間のリセット
-                                    //candy_balloon.SetActive(false);
-                                    //SceneManager.LoadScene("ResultScene");
-                }
-            }
-        }
+        
     }
 }
